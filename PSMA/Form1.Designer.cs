9e235cf7ac81dd018d7601edd9f6fb6f3dcceddc
ricardo-alexis <ricardoav0907@gmail.com>
2021-10-25 22:13:47 -0500

    PROBLEMA RESUELTO

---
+++ PSMA/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace PSMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "CASOS ACTIVOS";
+            this.columnHeader1.Width = 108;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PAIS";
+            this.columnHeader2.Width = 95;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ULTIMA ACTUALIZACIÓN";
+            this.columnHeader3.Width = 140;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "NUEVOS CASOS";
+            this.columnHeader4.Width = 99;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "NUEVOS DECESOS";
+            this.columnHeader5.Width = 116;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "TOTAL DE CASOS";
+            this.columnHeader6.Width = 110;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "TOTAL DE MUERTES";
+            this.columnHeader7.Width = 125;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "TOTAL DE RECUPERADOS";
+            this.columnHeader8.Width = 129;
+            // 
+            // listView1
+            // 
+            this.listView1.BackgroundImage = global::PSMA.Properties.Resources._1;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(11, 35);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(921, 613);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PSMA.Properties.Resources._1;
+            this.ClientSize = new System.Drawing.Size(944, 681);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ PSMA/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Globalization;
+using Newtonsoft.Json.Converters;
+
+namespace PSMA
+{
+    public partial class Form1 : Form
+    {
+        HttpClient client = new HttpClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await GetTodoItems();
+
+        }
+        private async Task GetTodoItems()
+        {
+            string response = await client.GetStringAsync("https://covid-19.dataflowkit.com/v1");
+            List<Model> covid = JsonConvert.DeserializeObject<List<Model>>(response);
+            foreach (var datos in covid)
+            {
+                string parsedActiveCasesText = datos.ActiveCasesText;
+                string parsedCountryText = datos.CountryText;
+                string parsedLastUpdate = datos.LastUpdate;
+                string parsedNewCasesText = datos.NewCasesText;
+                string parsedNewDeathsText = datos.NewDeathsText;
+                string parsedTotalCasesText = datos.TotalCasesText;
+                string parsedTotalDeathsText = datos.TotalDeathsText;
+                string parsedTotalRecoveredText = datos.TotalRecoveredText;
+                
+
+
+                string[] row = { parsedActiveCasesText, parsedCountryText, parsedLastUpdate,parsedNewCasesText,parsedNewDeathsText,parsedTotalCasesText,parsedTotalDeathsText,parsedTotalRecoveredText };
+                var listViewItems = new ListViewItem(row);
+                listView1.Items.Add(listViewItems);
+            }
+        }
+
+        private  void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+        public partial class Model
+        {
+            [JsonProperty("Active Cases_text", NullValueHandling = NullValueHandling.Ignore)]
+            public string ActiveCasesText { get; set; }
+
+            [JsonProperty("Country_text", NullValueHandling = NullValueHandling.Ignore)]
+            public string CountryText { get; set; }
+
+            [JsonProperty("Last Update", NullValueHandling = NullValueHandling.Ignore)]
+            public string LastUpdate { get; set; }
+
+            [JsonProperty("New Cases_text", NullValueHandling = NullValueHandling.Ignore)]
+            public string NewCasesText { get; set; }
+
+            [JsonProperty("New Deaths_text", NullValueHandling = NullValueHandling.Ignore)]
+            public string NewDeathsText { get; set; }
+
+            [JsonProperty("Total Cases_text", NullValueHandling = NullValueHandling.Ignore)]
+            public string TotalCasesText { get; set; }
+
+            [JsonProperty("Total Deaths_text", NullValueHandling = NullValueHandling.Ignore)]
+            public string TotalDeathsText { get; set; }
+
+            [JsonProperty("Total Recovered_text", NullValueHandling = NullValueHandling.Ignore)]
+            public string TotalRecoveredText { get; set; }
+        }
+    }
+}
